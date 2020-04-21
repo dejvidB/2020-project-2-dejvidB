1247ec22a6f0ec8f5b1900b44f83f8c9a30259d2
Dejvid Bushi <dejvid.bushi@gmail.com>
2020-04-21 20:03:28 +0300

    Use efficient heapify in Pq using Heap

--- modules/UsingHeap/ADTPriorityQueue.c
+++ modules/UsingHeap/ADTPriorityQueue.c
@@ -104,22 +104,25 @@ static void bubble_down(PriorityQueue pqueue, int node_id) {
 
 // Αρχικοποιεί το σωρό από τα στοιχεία του vector values.
 
-static void naive_heapify(PriorityQueue pqueue, Vector values) {
-	// Απλά κάνουμε insert τα στοιχεία ένα ένα.
-	// TODO: υπάρχει πιο αποδοτικός τρόπος να γίνει αυτό!
-	int size = vector_size(values);
-	for (int i = 0; i < size; i++)
-		pqueue_insert(pqueue, vector_get_at(values, i));
-}
-
-// static void efficient_heapify(PriorityQueue pqueue, Vector values){
-// 	for (int i = 0; i < vector_size(values); i++)
-// 		vector_insert_last(pqueue->vector, vector_get_at(values, i));
-
-// 	for(int i = pqueue_size(pqueue) / 2; i >= 1; i--){
-// 		bubble_down(pqueue, i);
-// 	}
+// static void naive_heapify(PriorityQueue pqueue, Vector values) {
+// 	// Απλά κάνουμε insert τα στοιχεία ένα ένα.
+// 	// TODO: υπάρχει πιο αποδοτικός τρόπος να γίνει αυτό!
+// 	int size = vector_size(values);
+// 	for (int i = 0; i < size; i++)
+// 		pqueue_insert(pqueue, vector_get_at(values, i));
 // }
+
+static void efficient_heapify(PriorityQueue pqueue, Vector values){
+	for (int i = 0; i < vector_size(values); i++){
+		PriorityQueueNode node = malloc(sizeof(PriorityQueueNode));
+		node->value = vector_get_at(values, i);
+		node->position = pqueue_size(pqueue) + 1;
+		vector_insert_last(pqueue->vector, node);
+	}
+	for(int i = pqueue_size(pqueue) / 2; i >= 1; i--){
+		bubble_down(pqueue, i);
+	}
+}
 
 // Συναρτήσεις του ADTPriorityQueue //////////////////////////////////////////////////
 
@@ -137,7 +140,7 @@ PriorityQueue pqueue_create(CompareFunc compare, DestroyFunc destroy_value, Vect
 
 	// Αν values != NULL, αρχικοποιούμε το σωρό.
 	if (values != NULL)
-		naive_heapify(pqueue, values);
+		efficient_heapify(pqueue, values);
 
 	return pqueue;
 }
